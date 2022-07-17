d0f9d47bacd8e266337e71ca32d434db2bb017e2
TogzogTheTits <21349674+TogzogTheTits@users.noreply.github.com>
2022-07-17 15:53:53 -0400

    final final final fix

--- oddsquares.cpp
+++ oddsquares.cpp
@@ -1,51 +1,52 @@
 #include <iostream>
 #include <cmath>
+#include <conio.h>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    int a = 0;
-    int b = 0;
-    int c = 0;
-    int limit = 0;
-    int test = 0;
-    int plus = 0;
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int limit = 0;
+        int test = 0;
+        int plus = 0;
 
-    cin >> limit;
+        cin >> limit;
 
-    while(test == 0)
-    {
-        a = 1;
-        
-        for(int i=0; i < plus; i++)
+        while(test == 0)
         {
-            b = a + (2 * i);
-
-            cout << b;
-
-            if(i!=plus-1)
+            a = 1;
+            
+            for(int i=0; i < plus; i++)
             {
-                cout << "+";
+                b = a + (2 * i);
+
+                cout << b;
+
+                if(i!=plus-1)
+                {
+                    cout << "+";
+                }
+
+                c += b;
             }
 
-            c += b;
+            if(plus!=0)
+            {
+                cout << "=" << c << "=" << sqrt(c) << "^2" << endl;
+            }
+
+            if(plus == limit)
+            {
+                test = 1;
+            }
+
+            c = 0;
+
+            plus++;
         }
 
-        if(plus!=0)
-        {
-            cout << "=" << c << "=" << sqrt(c) << "^2" << endl;
-        }
-
-        if(plus == limit)
-        {
-            test = 1;
-        }
-
-        c = 0;
-
-        plus++;
-    }
-
-    getchar();
+        getch();
 }
